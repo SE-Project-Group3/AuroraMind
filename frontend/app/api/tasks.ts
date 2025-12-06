1ef851471ed3383c9e1e6ef32f7ea5d5facb5cd7
Zhiqi Lu <zhiqilu8470@gmail.com>
2025-12-06 12:28:58 +0100

    feat: modify and delete tasks, task lists in the todo page

--- frontend/app/api/tasks.ts
+++ frontend/app/api/tasks.ts
@@ -15,10 +15,18 @@ export interface Task{
     id: string;
     user_id: string;
     task_list_id: string;
-    start_date?: string;
+    start_date: string;
     end_date: string;
     created_at?: string;
     updated_at?: string;
+}
+
+export interface TaskCreation {
+    "name": string;
+    "is_completed": boolean;
+    "task_list_id": string;
+    "start_date": string;
+    "end_date": string;
 }
 
 export async function getLists(): Promise<TaskList[]> {
@@ -31,6 +39,62 @@ export async function getLists(): Promise<TaskList[]> {
             },
         });
     return res.data.data;
+}
+
+export async function createList(name: string, goal_id = null) {
+    const token = localStorage.getItem("access_token");
+    const res = await axios.post(`${API_BASE}/api/v1/task-lists`,
+        {
+            "name": name,
+            "goal_id": goal_id,
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                 Authorization: token ? `Bearer ${token}` : ""
+            },
+        });
+    if (res.status === 201) {
+        return res.data.data;
+    }
+    console.log("failed to create task list")
+    return null;
+}
+
+export async function updateList(listId: string, name: string, goal_id = null) {
+    const token = localStorage.getItem("access_token");
+    const res = await axios.put(`${API_BASE}/api/v1/task-lists/${listId}`,
+        {
+            "name": name,
+            "goal_id": goal_id
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                 Authorization: token ? `Bearer ${token}` : ""
+            },
+        });
+    if (res.status === 200) {
+        return res.data.data;
+    }
+    console.log("failed to update list status")
+    return null;
+}
+
+export async function deleteList(listId: string) {
+    const token = localStorage.getItem("access_token");
+    const res = await axios.delete(`${API_BASE}/api/v1/task-lists/${listId}`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                 Authorization: token ? `Bearer ${token}` : ""
+            },
+        });
+    if (res.status === 200) {
+        return res.data.data;
+    }
+    console.log("failed to delete list")
+    return null;
 }
 
 export async function getTasks(listId: string): Promise<Task[]> {
@@ -66,4 +130,39 @@ export async function updateTask(task: Task) {
     }
     console.log("failed to update task status")
     return null;
+}
+
+export async function createTask(task: TaskCreation) {
+    const token = localStorage.getItem("access_token");
+    const res = await axios.post(`${API_BASE}/api/v1/tasks`,
+        {
+            "name": task.name,
+            "is_completed": task.is_completed,
+            "task_list_id": task.task_list_id,
+            "start_date": task.start_date,
+            "end_date": task.end_date,
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                 Authorization: token ? `Bearer ${token}` : ""
+            },
+        });
+    if (res.status === 201) {
+        return res.data.data;
+    }
+    console.log("failed to create task")
+    return null;
+}
+
+export async function deleteTask(taskId: string): Promise<boolean> {
+    const token = localStorage.getItem("access_token");
+    const res = await axios.delete(`${API_BASE}/api/v1/tasks/${taskId}`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                 Authorization: token ? `Bearer ${token}` : ""
+            },
+        });
+    return res.data.data === true;
 }
